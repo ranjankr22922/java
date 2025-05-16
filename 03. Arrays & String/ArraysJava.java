0013fee130e35ed444f69eab1139ecb3e542573a
Ranjan Kumar <rajkumar40317306@gmail.com>
2025-05-16 12:56:41 +0530

    Update ArraysJava.java - 12:56:40 16-05-2025

--- 03. Arrays & String/ArraysJava.java
+++ 03. Arrays & String/ArraysJava.java
@@ -39,9 +39,19 @@
 // }
 
 //4
-// 3 
+
+import java.util.Arrays;
+
 public class ArraysJava {
     public static void main(String[] args) {
-
+        int[] a = new int[3];
+        int x = 10;
+        for (int i = 0; i < a.length; i++) {
+            a[i] = x;
+            x += 10;
+        }
+        System.out.println(a); // Prints the memory address/reference of the array
+        System.out.println(Arrays.toString(a)); // [10, 20, 30]
+        System.out.println(a[3]); // ArrayIndexOutOfBoundsException
     }
 }
