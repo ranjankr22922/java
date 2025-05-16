0943d28bbbfc90a8b9d1fab2cd71100cd891a3a0
Ranjan Kumar <rajkumar40317306@gmail.com>
2025-05-16 17:28:23 +0530

    Update ArraysJava.java - 17:28:23 16-05-2025

--- 03. Arrays & String/ArraysJava.java
+++ 03. Arrays & String/ArraysJava.java
@@ -7,7 +7,9 @@
 // System.out.println(a[2]);
 // }
 // }
+
 // ------------------------------------------------------
+
 // Ways to create an array
 
 // 1
@@ -38,20 +40,64 @@
 // }
 // }
 
-//4
+// 4
+// import java.util.Arrays;
 
-import java.util.Arrays;
+// public class ArraysJava {
+// public static void main(String[] args) {
+// int[] a = new int[3];
+// int x = 10;
+// for (int i = 0; i < a.length; i++) {
+// a[i] = x;
+// x += 10;
+// }
+// System.out.println(a); // Prints the memory address/reference of the array
+// System.out.println(Arrays.toString(a)); // [10, 20, 30]
+// System.out.println(a[3]); // ArrayIndexOutOfBoundsException
+// }
+// }
+
+// -------------------------------------------------------------------------------
+
+// []a vs a[]
+// public class ArraysJava {
+// public static void main(String[] args) {
+
+// int[] a = new int[4];
+
+// int b[] = new int[4];
+
+// int[] c;
+// c = new int[4];
+
+// int d[];
+// d = new int[4];
+
+// int e[], f;
+// e = new int[4];
+// f = 10;
+
+// int[] g, h;
+// g = new int[4];
+// // h = 10; // error: incompatible types: int cannot be converted to int[]
+// }
+// }
+
+// -------------------------------------------------------------------------------
+
+// Array working and Types
+
+//Array is a collection of similar data types i.e. homogeneous.
+//Uninitialized array elements have 0 values
 
 public class ArraysJava {
     public static void main(String[] args) {
-        int[] a = new int[3];
-        int x = 10;
+        int[] a = new int[4];
+        boolean b[] = new boolean[1];
+        // int[] c = { 10, 4.5 }; // Incompatible types: Float not allowed in int[]
         for (int i = 0; i < a.length; i++) {
-            a[i] = x;
-            x += 10;
+            System.out.print(a[i] + " "); // 0 0 0 0
         }
-        System.out.println(a); // Prints the memory address/reference of the array
-        System.out.println(Arrays.toString(a)); // [10, 20, 30]
-        System.out.println(a[3]); // ArrayIndexOutOfBoundsException
+        System.out.println(b[0]); // false
     }
 }
