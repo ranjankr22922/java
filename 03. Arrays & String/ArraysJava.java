4eeaea5a6f9874343067d528ec9ee5e69ce6325b
Ranjan Kumar <rajkumar40317306@gmail.com>
2025-05-16 12:03:00 +0530

    Update ArraysJava.java - 12:03:00 16-05-2025

--- 03. Arrays & String/ArraysJava.java
+++ 03. Arrays & String/ArraysJava.java
@@ -1,1 +1,47 @@
-// lets do thisdfjbgdbgjfgvksf11
+// public class ArraysJava {
+// public static void main(String[] args) {
+// int[] a = { 10, 20, 30, 40 };
+// System.out.println(a.length);
+// a[2] = 50;
+// System.out.println(a[1]);
+// System.out.println(a[2]);
+// }
+// }
+// ------------------------------------------------------
+// Ways to create an array
+
+// 1
+// public class ArraysJava {
+// public static void main(String[] args) {
+// int[] a;
+// a = new int[3];
+// a[0] = 10;
+// a[1] = 20;
+// a[2] = 30;
+// }
+// }
+
+// 2
+// public class ArraysJava {
+// public static void main(String[] args) {
+// int[] a = new int[3];
+// a[0] = 10;
+// a[1] = 20;
+// a[2] = 30;
+// }
+// }
+
+// 3
+// public class ArraysJava {
+// public static void main(String[] args) {
+// int[] a = { 10, 20, 30 };
+// }
+// }
+
+//4
+// 3 
+public class ArraysJava {
+    public static void main(String[] args) {
+
+    }
+}
