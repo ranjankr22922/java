6d95831a17a16f083833a1111c8e52e9f7441cab
Ranjan Kumar <rajkumar40317306@gmail.com>
2025-05-20 00:20:40 +0530

    Update StringsJava.java - 00:20:40 20-05-2025

--- 03. Arrays & String/StringsJava.java
+++ 03. Arrays & String/StringsJava.java
@@ -1,6 +1,10 @@
+// String is a set of characters(16 bit)
+// Java uses UTF-16 encoding for Strings
+// Strings are immutable in Java
+
 public class StringsJava {
 
     public static void main(String[] args) {
-        // hi
+        System.out.println("Hi");
     }
 }
