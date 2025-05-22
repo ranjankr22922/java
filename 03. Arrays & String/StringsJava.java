d5bcd51ba678671f5dda05213d4c1bdb54142917
Ranjan Kumar <rajkumar40317306@gmail.com>
2025-05-23 00:33:31 +0530

    Update StringsJava.java - 00:33:30 23-05-2025

--- 03. Arrays & String/StringsJava.java
+++ 03. Arrays & String/StringsJava.java
@@ -1,14 +1,15 @@
-// String is a set of characters(16 bit)
-// Java uses UTF-16 encoding for Strings
+// // String is a set of characters(16 bit)
+// // Java uses UTF-16 encoding for Strings
 
-// There are 4 ways to create Strings in Java
+// // There are 4 ways to create Strings in Java
 
-// 1. Character Array / ArrayList
-// char[] arr = {'a','e','i','o','u'};
+// // 1. Character Array / ArrayList
+// // char[] arr = {'a','e','i','o','u'};
 
-// 2. String Class - Immutable Strings
-// 3. StringBuffer Class - Mutable Class : Multithreaded (Thread Safe)
-// 4. StringBuilder Class - Mutable Class : Single Threaded
+// // 2. String Class - Immutable Strings
+// // 3. StringBuilder Class - Mutable Class : Single Threaded
+// // 4. StringBuffer Class - Mutable Class : Multithreaded Synchronized Methods
+// // and Thread Safe
 
 // public class StringsJava {
 
@@ -55,6 +56,18 @@
 
 // System.out.println(str1.equals(str2)); // false - for and For are different.
 
+// String str3 = "Coders";
+// String str4 = str3;
+
+// System.out.println(str3 == str4); // true - Both refers to same obj.
+// System.out.println(str3.equals(str4)); // true - same content.
+
+// str3 = "d";
+// System.out.println(str3);
+// System.out.println(str4);
+
+// System.out.println(str3 == str4); // false - different references.
+// System.out.println(str3.equals(str4)); // false - different content.
 // }
 // }
 
@@ -133,41 +146,80 @@
 
 // public class StringsJava {
 
-//     public static void main(String[] args) {
-//         String s1 = "geeks";
-//         String s2 = s1;
+// public static void main(String[] args) {
+// String s1 = "geeks";
+// String s2 = s1;
 
-//         s1 = s1 + "forgeeks"; // OR s1 = s1.concat("forgeeks");
-//         // creates a new object without modifying original object.
-//         // s2 still refers to the original object.
+// s1 = s1 + "forgeeks"; // OR s1 = s1.concat("forgeeks");
+// // creates a new object without modifying original object.
+// // s2 still refers to the original object.
 
-//         System.out.println(s1);
-//         System.out.println(s1 == s2); // false
-//         System.out.println(s2);
-//     }
+// System.out.println(s1);
+// System.out.println(s1 == s2); // false
+// System.out.println(s2);
+// }
 // }
 
 // ----------------------------------------------------------------
 
-// Changing the Strings create new objects.
-// StringBuilder & StringBuffer - Mutable Strings avoid heap garbage
+// // Changing the Strings create new objects.
+// // StringBuilder & StringBuffer - Mutable Strings avoid heap garbage.
+// // These are classes not strings whose obj are created using 'new' keyword.
+// // They are not strings but can use all the operations of strings.
+
+// // StringBuilder - Available from Java 1.5
+// // StringBuffer - Available from Java 1.0
+
+// public class StringsJava {
+
+// public static void main(String[] args) {
+
+// StringBuilder sb = new StringBuilder(); // default constructor with size 16
+// StringBuilder sb0 = new StringBuilder(10); // size = 10
+
+// System.out.println(sb.capacity());
+// System.out.println(sb0.capacity());
+
+// StringBuilder sb1 = new StringBuilder("geeks");
+// StringBuilder sb2 = sb1;
+// StringBuilder sb3 = new StringBuilder("Hello World");
+// StringBuilder sb4 = new StringBuilder("malayalam");
+
+// sb1.append("forgeeks"); // length is 13 which is less than capacity 21(16+5)
+// // so no capacity increase needed.
+
+// System.out.println(sb1 == sb2); // true
+// System.out.println(sb1);
+// System.out.println(sb2);
+// System.out.println(sb1.capacity()); // 21
+// System.out.println(sb3.capacity()); // 27 (16+11)
+
+// sb3.insert(11, " Java !!!"); // Insert inputs a value at a specific index
+// System.out.println(sb3);
+
+// sb3.replace(11, 17, " Python ");
+// System.out.println(sb3);
+
+// sb3.delete(11, 18); // 11 is inclusive and 18 is exclusive
+// System.out.println(sb3);
+
+// sb4.reverse();
+// System.out.println(sb4); // Palindrome String
+
+// System.out.println(sb4.charAt(4));
+// sb4.setCharAt(4, 'p');
+// System.out.println(sb4);
+
+// String myString = sb4.toString(); // Convert StringBuilder to String
+// // Any changes made to myString doesn't affect sb4 and creates a new obj.
+
+// }
+// }
+
+// ----------------------------------------------------------------
 
 public class StringsJava {
 
     public static void main(String[] args) {
-
-        StringBuilder sb = new StringBuilder(); // default constructor with size 16
-        StringBuilder sb0 = new StringBuilder(10); // size = 10
-
-        System.out.println(sb.capacity());
-        System.out.println(sb0.capacity());
-
-        StringBuilder sb1 = new StringBuilder("geeks");
-        StringBuilder sb2 = sb1;
-
-        sb1.append("forgeeks");
-
-        System.out.println(sb1 == sb2); // true
-
     }
 }
